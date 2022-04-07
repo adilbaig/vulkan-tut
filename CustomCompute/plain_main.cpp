ece07a0b23f1c1f4e613173484d76d244caa2a36
adil <adil.baig@aidezigns.com>
2022-04-08 01:17:38 +0400

    plain_main is now multithreaded. Update CMake to allow building plain_main.

--- CustomCompute/plain_main.cpp
+++ CustomCompute/plain_main.cpp
@@ -1,25 +1,51 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <thread>
 
-const uint32_t NumElements = 300 * 1000000;
+const auto n =  std::thread::hardware_concurrency();
+const uint32_t NumElements = 1000 * 1000000;
+std::vector<uint32_t> InBuffer(NumElements);
+std::vector<uint32_t> OutBuffer(NumElements);
+
+void doSomething(uint32_t I, uint32_t end)
+{
+  for (uint32_t I = 0; I < end; ++I)
+  {
+    OutBuffer[I] = InBuffer[I] * 2;
+  }
+}
 
 int main()
 {
-  std::vector<uint32_t> InBuffer(NumElements);
-  std::vector<uint32_t> OutBuffer(NumElements);
   for (int32_t I = 0; I < NumElements; ++I)
   {
     InBuffer[I] = I;
   }
 
-  for (uint32_t I = 0; I < NumElements; ++I)
+  std::cout << "Starting " << n << " threads" << std::endl;
+  
+  std::vector<std::thread> threads(n);
+  uint chunkSize = NumElements/n;
+
+  for (int i = 0; i < n; i++)
   {
-    OutBuffer[I] = InBuffer[I] * 2;
+    auto start = i * chunkSize;
+    auto end = start + chunkSize;
+    if (end > NumElements) {
+      end = NumElements;
+    }
+
+    threads[i] = std::thread(doSomething, start, end);
+  }
+
+  for (auto &th : threads)
+  {
+    th.join();
   }
 
   // Sssh!
-  for (uint32_t I = NumElements-100; I < NumElements; ++I)
+  for (uint32_t I = NumElements - 100; I < NumElements; ++I)
   {
     std::cout << InBuffer[I] << "(" << OutBuffer[I] << ")\n";
   }
