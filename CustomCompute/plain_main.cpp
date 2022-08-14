8cbe796428cc27c2e7633f62cc7c3a74576ee2e5
adil <adil.baig@aidezigns.com>
2022-08-15 00:02:16 +0400

    Fix chunking in plain_main.cpp

--- CustomCompute/plain_main.cpp
+++ CustomCompute/plain_main.cpp
@@ -3,14 +3,14 @@
 #include <vector>
 #include <thread>
 
-const auto n =  std::thread::hardware_concurrency();
+const auto n = std::thread::hardware_concurrency();
 const uint32_t NumElements = 300 * 1000000;
 std::vector<uint32_t> InBuffer(NumElements);
 std::vector<uint32_t> OutBuffer(NumElements);
 
-void doSomething(uint32_t I, uint32_t end)
+void doSomething(uint32_t start, uint32_t end)
 {
-  for (uint32_t I = 0; I < end; ++I)
+  for (uint32_t I = start; I < end; ++I)
   {
     OutBuffer[I] = InBuffer[I] * 2;
   }
@@ -18,21 +18,24 @@ void doSomething(uint32_t I, uint32_t end)
 
 int main()
 {
-  for (int32_t I = 0; I < NumElements; ++I)
+  // Fill in NumElements of numbers to InBuffer
+  for (uint32_t I = 0; I < NumElements; ++I)
   {
     InBuffer[I] = I;
   }
 
   std::cout << "Starting " << n << " threads" << std::endl;
-  
-  std::vector<std::thread> threads(n);
-  uint chunkSize = NumElements/n;
 
-  for (int i = 0; i < n; i++)
+  std::vector<std::thread> threads(n);
+  uint chunkSize = NumElements / n;
+
+  // Start `n` threads, each will process a fixed size chunk of the input
+  for (uint32_t i = 0; i < n; i++)
   {
     auto start = i * chunkSize;
     auto end = start + chunkSize;
-    if (end > NumElements) {
+    if (end > NumElements)
+    {
       end = NumElements;
     }
 
@@ -44,8 +47,9 @@ int main()
     th.join();
   }
 
-  // Sssh!
-  for (uint32_t I = NumElements - 100; I < NumElements; ++I)
+  // Just print the last X values!
+  auto X = 5; 
+  for (uint32_t I = NumElements - X; I < NumElements; ++I)
   {
     std::cout << InBuffer[I] << "(" << OutBuffer[I] << ")\n";
   }
